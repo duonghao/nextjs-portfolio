fa593f87f071e76fff805bbaf2564ff512f6670a
Hao Duong <hao.duong11@gmail.com>
2022-11-25 20:14:47 +1030

    Added links to navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className='flex justify-around'>
-        <div className='w-9 h-9 border rounded-full shadow-md'></div>
+        <Link href='/' className='block'>
+          <div className='w-9 h-9 border rounded-full shadow-md'></div>
+        </Link>
         <ul className='flex justify-around items-center rounded-full border bg-white w-1/3 shadow-md'>
             <li className='hover:text-sky-500'>
-              About
+              <Link href="/about">About</Link>
             </li>
             <li className='hover:text-sky-500'>
-              Projects
+              <Link href="/projects">Projects</Link>
             </li>
         </ul>
         <div className='w-9 h-9 border rounded-full shadow-md'></div>
