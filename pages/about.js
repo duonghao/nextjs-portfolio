95e6e0c72e2c93b4fe4f672b383e7f272014f76f
Hao Duong <hao.duong11@gmail.com>
2022-11-25 20:15:09 +1030

    Added new pages

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from "../components/layout";
+import Navbar from "../components/navbar";
+
+export default function About() {
+    return (
+        <Layout>
+            <Navbar></Navbar>
+            <div>This is the About page!</div>
+        </Layout>
+    )
+}
---
+++ pages/projects.js
@@ -0,0 +1,11 @@
+import Layout from "../components/layout";
+import Navbar from "../components/navbar";
+
+export default function Projects() {
+    return (
+        <Layout>
+            <Navbar></Navbar>
+            <div>This is the Projects page!</div>
+        </Layout>
+    )
+}
